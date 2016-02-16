4de25779b4aac7c420bbb3e6386af02e33a50ed4
Holi0317 <holliswuhollis@gmail.com>
2016-02-16 21:35:45 +0800

    [FIX] utils not imported.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from .utils.logger import logger
+from utils.logger import logger
 
 MORNING_URL = 'http://mail.stmarks.edu.hk/~smssmf/morning.htm'
 ASSEMBLY_URL = 'http://mail.stmarks.edu.hk/~smssmf/assembly.htm'
