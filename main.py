c9288cb04eb1c88e6a1ca2ead3eb7997a0086f32
Holi0317 <holliswuhollis@gmail.com>
2016-02-16 21:30:07 +0800

    [:octocat:] Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from .utils.logger import logger
+
+MORNING_URL = 'http://mail.stmarks.edu.hk/~smssmf/morning.htm'
+ASSEMBLY_URL = 'http://mail.stmarks.edu.hk/~smssmf/assembly.htm'
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ utils/logger.py
@@ -0,0 +1,10 @@
+import logging
+
+logger = logging.getLogger('duty-notify')
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    '[%(asctime)s] [%(name)s / %(levelname)s] %(message)s',
+    '%H:%M:%S')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
